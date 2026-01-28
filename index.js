e87796f0dea94918e8db1cc3ef596d64d63d7eb5
shahzahandev <shahzahansiraj516@gmail.com>
2026-01-29 01:05:48 +0600

    set Express js in this project

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const http = require("node:http");
+// const http = require("node:http");
 
-const server = http.createServer((req, res) => {
-    // console.log(req.headers);
-    // console.log(req.method);
-    // console.log(req.url);
-    if(req.method == 'GET' && req.url == '/home'){ 
-     res.writeHead( 200, {"Content-Type" : "text/html"});
-     res.end('<h1>siraj<h1/>')
-    }
-})
+// const server = http.createServer((req, res) => {
+//     // console.log(req.headers);
+//     // console.log(req.method);
+//     // console.log(req.url);
+//     if(req.method == 'GET' && req.url == '/home'){ 
+//      res.writeHead( 200, {"Content-Type" : "text/html"});
+//      res.end('<h1>siraj<h1/>')
+//     }
+// })
 
-server.listen(5000, 'localhost', () => {
-    console.log("Server running......");
-})
+// server.listen(5000, 'localhost', () => {
+//     console.log("Server running......");
+// })
+
+const express = require('express');
+const app = express();
+
+app.use('/home', (req, res) => {
+    res.send('<h1>Hellow world-2</h1>');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running.........');
+});
 
 
 
